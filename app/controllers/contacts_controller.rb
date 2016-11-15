1f66952efc8ee08655610f733c201422e7d97909
Jessica Genualdi <jessi.genualdi@fcb.com>
2016-11-14 20:04:48 -0600

    adding restful routes

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -2,7 +2,40 @@ class ContactsController < ApplicationController
   def contact_method
     @contact = Contact.first
 
-    @all_contacts = Contact.all
+    @contacts = Contact.all
     render 'contact.html.erb'
+  end
+
+  def index 
+    @contacts = Contact.all
+    render "index.html.erb"
+  end
+
+  def new
+    render "new.html.erb"
+  end
+
+  def create
+    @contact = Contact.new(first_name: params[:first_name], 
+      last_name: params[:last_name], 
+      email: params[:email], 
+      phone_number: params[:phone_number])
+    @contact.save
+  end
+
+  def show
+    @contact = Contact.find_by(id: params[:id])
+    render "show.html.erb"
+  end
+
+  def edit
+    @contact = Contact.find_by(id: params[:id])
+    render "edit.html.erb"
+  end
+
+  def update
+  end
+
+  def destroy
   end
 end
